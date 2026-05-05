579fb9787a00e7df8c42b3407817461003ecc0c5
yoonseo <lys8167@gmail.com>
2026-05-05 17:56:11 +0900

    feat: Swagger에 JWT bearer authentication 설정 추가

--- apps/backend/src/main/java/com/sos/backend/global/common/config/SwaggerConfig.java
+++ apps/backend/src/main/java/com/sos/backend/global/common/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.sos.backend.global.common.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +13,13 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+            .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+            .components(new io.swagger.v3.oas.models.Components()
+                .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                    .name("Authorization")
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")))
             .info(new Info()
                 .title("S.O.S API")
                 .description("Safe or Scam 백엔드 API 명세서")
